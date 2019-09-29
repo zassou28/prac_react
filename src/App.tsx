187891f4371ac73522a84e3a9c75fb53025008ca
zassou28 <pngnptpt28@gmail.com>
2019-09-29 19:11:44 +0900

    add: setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useCallback } from 'react';
+import { createStore } from 'redux';
 import './App.css';
+import styled from 'styled-components';
+
+// styled-component
+const Title = styled.h1`
+  color: gray;
+`;
 
 const App: React.FC = () => {
+
+  const [first_name, changeFirst] = useState("");
+  const [last_name, changeTLast] = useState("");
+
+  const resetName = () => {
+    changeFirst("");
+    changeTLast("");
+  };
+  
+  const changeF = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      changeFirst(event.target.value)
+    },
+    [first_name]
+  )
+  const changeL = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      changeTLast(event.target.value)
+    },
+    [last_name]
+  )
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Title>練習</Title>
+      <input placeholder="FirstName" value={first_name} onChange={changeF}></input>
+      <br/>
+      <input placeholder="LastName" value={last_name} onChange={changeL}></input>
+      <br/>
+      <button onClick={resetName}>push</button>
+      <br/>
+      <p>FirstName : {first_name}</p>
+      <p>LastName : {last_name}</p>
+
     </div>
   );
 }
